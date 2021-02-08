99b30c482dd089d47216a637da11524c9e72219f
Nadezda Chikurova <nadezdachikurova@Nadezdas-MacBook-Air.local>
2021-02-07 21:32:48 -0500

    changes

--- website/main.js
+++ website/main.js
@@ -4,3 +4,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+// const secondSection = document.getElementsByClassName('second-section')[0]
+// const projectsButton = document.getElementsByClassName('projects-button')[0]
+
+// projectsButton.addEventListener('click', () => {
+//     secondSection.classList.toggle('active')
+// })
